3cbb47f4878f76ac0618d0eec37dcb943210eaca
keith(MacBookPro) <ka33145@googlemail.com>
2018-03-26 18:32:11 +0100

    Whitelisted restaurant name and description for mass assignment

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    //whitelist fields for mass assignment
+    protected $fillable =['name','description'];
+
 }
