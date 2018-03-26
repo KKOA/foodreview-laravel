3ec09a29fa17883146a699c54a74af84d20e6c48
keith(MacBookPro) <ka33145@googlemail.com>
2018-03-26 23:21:56 +0100

    Corrected typo in inc.messages

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -2,7 +2,7 @@
 @if ($errors->any())
   <div class='row'>
       <div class='col-md-offset-1 col-md-10'>
-        <div class="panel panel-danger">
+        <div class="panel panel-danger error-panel">
           <div class="panel-heading">
               <h2 class="panel-title text-center">
                   <?php $error_count = count($errors->all());
@@ -24,14 +24,16 @@
 
 @if(session('success'))
   <div class='row'>
-    <div class="panel panel-success success-panel col-md-offset-1 col-md-10">
-      <div class="panel-heading">
-        <h2 class="panel-title text-center">
-          Success
-        </h2>
-      </div>
-      <div class="panel-body">
-        <h4>{{session('success')}}</h4>
+    <div class="col-md-offset-1 col-md-10">
+      <div class="panel panel-success success-panel">
+        <div class="panel-heading">
+          <h2 class="panel-title text-center">
+            Success
+          </h2>
+        </div>
+        <div class="panel-body">
+          <h4>{{session('success')}}</h4>
+        </div>
       </div>
     </div>
   </div>
@@ -39,14 +41,16 @@
 
 @if(session('error'))
   <div class='row'>
-    <div class="panel panel-success error-panel col-md-offset-1 col-md-10">
-      <div class="panel-heading">
-        <h2 class="panel-title text-center">
-          Error
-        </h2>
-      </div>
-      <div class="panel-body">
-        <h4>{{session('error')}}</h4>
+      <div class="col-md-offset-1 col-md-10">
+      <div class="panel panel-danger error-panel">
+        <div class="panel-heading">
+          <h2 class="panel-title text-center">
+            Error
+          </h2>
+        </div>
+        <div class="panel-body">
+          <h4>{{session('error')}}</h4>
+        </div>
       </div>
     </div>
   </div>
