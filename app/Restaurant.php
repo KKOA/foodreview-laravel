739b0d1009789089c1cfbee0ec30b7e7c25203a1
keith(MacBookPro) <ka33145@googlemail.com>
2018-03-27 17:51:58 +0100

    Added restaurant mutator method for name

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -9,4 +9,15 @@ class Restaurant extends Model
     //whitelist fields for mass assignment
     protected $fillable =['name','description'];
 
+    public function setNameAttribute($value){
+
+        /*
+        Mutator define the following format set[cased name of the column you wish to access]Attribute
+        E.g. For column first_name  function would called setFirstNameAttribute
+        This mutator will be automatically called when we attempt to set the 
+        value of the column attribute on the model
+        */
+
+        $this->attributes['name'] = ucwords($value);
+    }
 }
