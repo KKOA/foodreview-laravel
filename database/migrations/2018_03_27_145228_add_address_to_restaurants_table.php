04422a73c685416f72ddde54ace52d7d8f6920d0
keith(MacBookPro) <ka33145@googlemail.com>
2018-03-29 10:32:42 +0100

    Updated the address fields' order in the database

--- database/migrations/2018_03_27_145228_add_address_to_restaurants_table.php
+++ database/migrations/2018_03_27_145228_add_address_to_restaurants_table.php
@@ -14,11 +14,11 @@ class AddAddressToRestaurantsTable extends Migration
     public function up()
     {
         Schema::table('restaurants', function (Blueprint $table) {
-            $table->string('address1');
-            $table->string('address2')->nullable();
-            $table->string('city');
-            $table->string('county')->nullable();
-            $table->string('postcode');
+            $table->string('address1')->after('description');
+            $table->string('address2')->nullable()->after('address1');
+            $table->string('city')->after('address2');
+            $table->string('county')->nullable()->after('city');
+            $table->string('postcode')->after('county');
         });
     }
 
