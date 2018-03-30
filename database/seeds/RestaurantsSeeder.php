faa4d3cead9e12cbcc61704a0d0a4403b5c6f84c
keith(MacBookPro) <ka33145@googlemail.com>
2018-03-30 18:50:29 +0100

    Added more restaurants to Restaurant Seeder

--- database/seeds/RestaurantsSeeder.php
+++ database/seeds/RestaurantsSeeder.php
@@ -19,10 +19,120 @@ class RestaurantsSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
         Restaurant::firstOrCreate(
+            ['name'=>'El Greco'], //Greek
+            [
+                'name'          =>  'El Greco',
+                'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '27 Rother Street',
+                'address2'      =>  '',
+                'city'          =>  'Stratford-upon-Avon',
+                'county'        =>  '',
+                'postcode'      =>  'CV37 6QB'
+            ]
+        );
+        Restaurant::firstOrCreate(
+            ['name'=>'Reubens'], //Russina
+            [
+                'name'          =>  'Reubens',
+                'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '79 Baker Street',
+                'address2'      =>  '',
+                'city'          =>  'London',
+                'county'        =>  '',
+                'postcode'      =>  'W1U 6AG'
+            ]
+        );
+        Restaurant::firstOrCreate(
+            ['name'=>'Laduree UK'], //Swedish
+            [
+                'name'          =>  'Laduree UK',
+                'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '71 Burlington Arcade',
+                'address2'      =>  '',
+                'city'          =>  'London',
+                'county'        =>  '',
+                'postcode'      =>  'W1J 0QX'
+            ]
+        );
+        Restaurant::firstOrCreate(
+            ['name'=>'Sorrentino Restaurant'], //Italien
+            [
+                'name'          =>  'Sorrentino Restaurant',
+                'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '64 Gold Street',
+                'address2'      =>  '',
+                'city'          =>  'Northampton',
+                'county'        =>  '',
+                'postcode'      =>  'NN1 1RS'
+            ]
+        );
+        Restaurant::firstOrCreate(
+            ['name'=>'Makara'], //Turkish
+            [
+                'name'          =>  'Makara',
+                'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '28 Church Road',
+                'address2'      =>  '',
+                'city'          =>  'Hove',
+                'county'        =>  '',
+                'postcode'      =>  'BN3 2FN'
+            ]
+        );
+        Restaurant::firstOrCreate(
+            ['name'=>'Tenkaichi'], //Japanense
+            [
+                'name'          =>  'Tenkaichi',
+                'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '236 City Road',
+                'address2'      =>  '',
+                'city'          =>  'Cardiff',
+                'county'        =>  '',
+                'postcode'      =>  'CF24 3JJ'
+            ]
+        );
+
+        Restaurant::firstOrCreate(
+            ['name'=>'Bear & Billet'],//German
+            [
+                'name'          =>  'Bear & Billet',
+                'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '94 Lower Bridge Street',
+                'address2'      =>  '',
+                'city'          =>  'Chester',
+                'county'        =>  '',
+                'postcode'      =>  'CH1 1RU'
+            ]
+        );
+
+        Restaurant::firstOrCreate(
+            ['name'=>'Akamba'], //African
+            [
+                'name'          =>  'Akamba',
+                'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  'Tythe Barn Lane',
+                'address2'      =>  '',
+                'city'          =>  'Solihull',
+                'county'        =>  '',
+                'postcode'      =>  'B90 1PH'
+            ]
+        );
+        Restaurant::firstOrCreate(
+            ['name'=>'Bistro Jacques'],//French
+            [
+                'name'          =>  'Bistro Jacques',
+                'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'address1'      =>  '29 Claremount Street',
+                'address2'      =>  '',
+                'city'          =>  'Shrewsbury',
+                'county'        =>  '',
+                'postcode'      =>  'SY1 1RD'
+            ]
+        );
+        Restaurant::firstOrCreate( //American
             ['name'=>'Infamous Diner'],
             [
                 'name'          =>  'Infamous Diner',
-                'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
                 'address1'      =>  '3-5 Basil Chambers Nicholas Croft',
                 'address2'      =>  '',
                 'city'          =>  'Manchester',
@@ -32,7 +142,7 @@ class RestaurantsSeeder extends Seeder
         );
 
         Restaurant::firstOrCreate(
-            ['name'=>'Los Gatos'],
+            ['name'=>'Los Gatos'], //Spanish
             [
                 'name'          =>  'Los Gatos',
                 'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
@@ -45,7 +155,7 @@ class RestaurantsSeeder extends Seeder
         );
 
         Restaurant::firstOrCreate(
-            ['name'=>'Nur'],
+            ['name'=>'Nur'], //Egyptian
             [
                 'name'          =>  'Nur',
                 'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
@@ -58,7 +168,7 @@ class RestaurantsSeeder extends Seeder
         );
 
         Restaurant::firstOrCreate(
-            ['name'=>'Thai Garden'],
+            ['name'=>'Thai Garden'], //Thai
             [
                 'name'          =>  'Thai Garden',
                 'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
@@ -71,7 +181,7 @@ class RestaurantsSeeder extends Seeder
         );
 
         Restaurant::firstOrCreate(
-            ['name'=>'Amigos Mexican Restaurant'],
+            ['name'=>'Amigos Mexican Restaurant'], //Mexican
             [
                 'name'          =>  'Amigos Mexican Restaurant',
                 'description'   => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
@@ -84,7 +194,7 @@ class RestaurantsSeeder extends Seeder
         );
 
         Restaurant::firstOrCreate(
-            ['name'=>'Lebaneat'],
+            ['name'=>'Lebaneat'], //Lebanese
             [
                 'name'          =>  'Lebaneat',
                 'description'   =>  $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
@@ -97,63 +207,3 @@ class RestaurantsSeeder extends Seeder
         );
     }
 }
-
-// @restaurant5 = Restaurant.where(name: 'Infamous Diner').first_or_create(
-//     name: 'Infamous Diner',
-//     description: Faker::Lorem.paragraph(8, true, 2),
-//     address1: '3-5 Basil Chambers Nicholas Croft',
-//     address2: '',
-//     city:  'Manchester',
-//     county:  '',
-//     postcode: 'M4 1EY'
-//   )
-  
-//   @restaurant6 = Restaurant.where(name: 'Los Gatos').first_or_create(
-//     name: 'Los Gatos',
-//     description: Faker::Lorem.paragraph(7, true, 3),
-//     address1: '1-3 Devizes Road',
-//     address2: 'Old Town',
-//     city:  'Swindon',
-//     county:  '',
-//     postcode: 'SN4 4BJ'
-//   )
-  
-//   @restaurant7 = Restaurant.where(name: 'Nur').first_or_create(
-//     name: 'Nur',
-//     description: Faker::Lorem.paragraph(6, true, 4),
-//     address1: '1-3 Devizes Road',
-//     address2: '',
-//     city:  'Glasgow',
-//     county:  '',
-//     postcode: 'G5 9HR'
-//   )
-  
-//   @restaurant8 = Restaurant.where(name: 'Thai Garden').first_or_create(
-//     name: 'Thai Garden',
-//     description: Faker::Lorem.paragraph(2, true, 9),
-//     address1: '100 West Street',
-//     address2: '',
-//     city:  'Bristol',
-//     county:  '',
-//     postcode: 'BS3 3LR'
-//   )
-  
-//   @restaurant9 = Restaurant.where(name: 'Amigos Mexican Restaurant').first_or_create(
-//     name: 'Amigos Mexican Restaurant',
-//     description: Faker::Lorem.paragraph(5, true, 7),
-//     address1: '41 Friar Street',
-//     address2: '',
-//     city:  'Worchester',
-//     county:  '',
-//     postcode: 'WR1 2NA'
-//   )
-  
-//   @restaurant10 = Restaurant.where(name: 'Lebaneat').first_or_create(
-//     name: 'Lebaneat',
-//     description: Faker::Lorem.paragraph(4, true, 8),
-//     address1: '47 North Baliey',
-//     address2: '',
-//     city:  'Durham',
-//     county:  '',
-//     postcode: 'DH1 3ET'
-//   )
